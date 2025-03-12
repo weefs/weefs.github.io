ab97ee0b16ac4254f02c2aefee2b7e25a4b14d90
weefs <nat.kusuma@gmail.com>
2025-03-12 11:30:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function onButtonClick() {
     // alert("Button clicked!");
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
+    if (clicks == 100) {
+        alert("wow u got to 100!!! meow :3");
+    }
 }
 
 // const button = document.querySelector('button');
