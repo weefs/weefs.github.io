3013c8ed9c2e95b37cb3fe5d896cd8d5b4cdecb1
weefs <nat.kusuma@gmail.com>
2025-03-12 10:50:47 +0800

    button click counter

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+var clicks = 0;
+
 function onButtonClick() {
-    alert("Button clicked!");
+    // alert("Button clicked!");
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
 }
 
-const button = document.querySelector('button');
-button.addEventListener('click', onButtonClick);
+// const button = document.querySelector('button');
+// button.addEventListener('click', onButtonClick);
