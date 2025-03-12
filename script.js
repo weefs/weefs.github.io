52e537bad95fefd5d593602034646fe84f13c902
weefs <nat.kusuma@gmail.com>
2025-03-12 11:50:37 +0800

    gacha!

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 var clicks = 0;
+var stars = 0;
+var roll = 0;
 
 function onButtonClick() {
-    // alert("Button clicked!");
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
     if (clicks == 100) {
         alert("wow u got to 100!!! meow :3");
     }
+    stars = 0;
+    roll = Math.random();
+    while (roll <= 1) {
+        roll *= 2
+        stars += 1
+    }
+    document.getElementById("stars").innerHTML = stars;
+
 }
 
 // const button = document.querySelector('button');
